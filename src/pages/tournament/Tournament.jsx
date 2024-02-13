236f3b359175b6df816110cb976adc3234838b20
vivekpatil94 <vsoftinfotech@gmail.com>
2024-02-14 04:10:41 +0530

    Tournament Modification

--- src/pages/tournament/Tournament.jsx
+++ src/pages/tournament/Tournament.jsx
@@ -33,6 +33,8 @@ const Tournament = (props) => {
     startDate: "",
     endDate: "",
     joinFee: "",
+    priceMoney: "",
+    targetAudience: "",
   });
 
   useEffect(() => {
@@ -47,6 +49,8 @@ const Tournament = (props) => {
       startDate: "",
       endDate: "",
       joinFee: "",
+      priceMoney: '',
+      targetAudience: "",
     });
   };
 
@@ -63,7 +67,8 @@ const Tournament = (props) => {
 
     try {
       const response = await axios.post(
-        "https://api1.d-ludo.com/api/Tournaments",
+        //  "https://api1.d-ludo.com/api/Tournaments",
+        "https://api1.d-ludo.com/api/Admin/Tournament/CreateTournament",
         formData
       );
 
@@ -92,7 +97,9 @@ const Tournament = (props) => {
 
   const fetchTournaments = async () => {
     try {
-      const response = await axios.get("https://api1.d-ludo.com/api/Tournaments");
+      //const response = await axios.get("https://api1.d-ludo.com/api/Tournaments");
+      const response = await axios.get("https://api1.d-ludo.com/api/admin/Tournament/GetTournaments");
+
       setTournaments(response.data);
     } catch (error) {
       console.error("Error fetching tournaments:", error);
@@ -110,26 +117,91 @@ const Tournament = (props) => {
   const handleCloseList = () => {
     setShowTournaments(false);
   };
-  const handleEdit = (tournament) => {
-    // Implement your logic to handle editing the tournament
-    console.log("Editing tournament:", tournament);
+
+  // Edit Code Starts
+  const [editOpen, setEditOpen] = useState(false);
+
+  const handleEditOpen = (tournament) => {
+    setFormData({ ...tournament });
+    setEditOpen(true);
   };
 
-  const handleCopy = (tournament) => {
-    // Implement your logic to handle copying the tournament
-    console.log("Copying tournament:", tournament);
+  const handleEditClose = () => {
+    setEditOpen(false);
   };
 
-  const handleDelete = (tournament) => {
-    // Implement your logic to handle deleting the tournament
-    console.log("Deleting tournament:", tournament);
+  const handleEditFormSubmit = async (event) => {
+    event.preventDefault();
+    const { title, startDate, endDate, joinFee, priceMoney } = formData;
+
+    try {
+      const response = await axios.put(
+        "https://api1.d-ludo.com/api/Admin/Tournament/EditTournament",
+        formData
+      );
+
+      if (response.status === 200) {
+        props.showSuccess("Tournament edited successfully!");
+      } else {
+        props.showError(response.data.message + "error");
+      }
+    } catch (error) {
+      props.showError("An error occurred. Please try again later.");
+    } finally {
+      handleEditClose();
+    }
   };
+
+
+  //  Handle Delete 
+  const [deleteConfirmationOpen, setDeleteConfirmationOpen] = useState(false);
+  const [tournamentToDelete, setTournamentToDelete] = useState(null);
+
+  const handleDeleteConfirmation = async (tournament) => {
+    const confirmed = window.confirm("Are you sure you want to delete this tournament?");
+    if (confirmed) {
+      handleDelete(tournament);
+    }
+  };
+  const handleDeleteConfirmationClose = () => {
+    setTournamentToDelete(null);
+    setDeleteConfirmationOpen(false);
+  };
+
+  const handleDelete = async (tournament) => {
+    try {
+      const response = await axios.delete(
+        `https://api1.d-ludo.com/api/Admin/Tournament/DeleteTournament/${tournament.id}`
+      );
+  
+      if (response.status === 200) {
+        props.showSuccess("Tournament deleted successfully!");
+        // You may also want to remove the deleted tournament from the state
+      } else {
+        props.showError(response.data.message + "error");
+      }
+    } catch (error) {
+      props.showError("An error occurred. Please try again later.");
+    }
+  };
+
+
+  
+  //  Handle Delete 
+
+
+
+
 
   const formatDateTimeString = (dateTimeString) => {
     const date = new Date(dateTimeString);
     const options = { year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric', minute: '2-digit' };
     return date.toLocaleString('en-US', options);
   };
+
+
+
+
   return (
     <>
       <Box
@@ -157,6 +229,119 @@ const Tournament = (props) => {
           </Button>
         </div>
       </Box>
+
+      {/* Edit Dialog Box */}
+
+      <Dialog
+        open={editOpen}
+        onClose={handleEditClose}
+        PaperProps={{
+          component: "form",
+          onSubmit: handleEditFormSubmit,
+        }}
+      >
+        <DialogTitle>Edit Tournament</DialogTitle>
+        <DialogContent>
+          <TextField
+            autoFocus
+            required
+            margin="dense"
+            id="title"
+            name="title"
+            label="Title"
+            type="text"
+            fullWidth
+            variant="standard"
+            value={formData.title}
+            onChange={handleInputChange}
+          />
+          <TextField
+            required
+            margin="dense"
+            id="startDate"
+            name="startDate"
+            label="Start Date"
+            type="datetime-local"
+            fullWidth
+            variant="standard"
+            InputLabelProps={{
+              shrink: true,
+            }}
+            value={formData.startDate}
+            onChange={handleInputChange}
+          />
+          <TextField
+            required
+            margin="dense"
+            id="endDate"
+            name="endDate"
+            label="End Date"
+            type="datetime-local"
+            fullWidth
+            variant="standard"
+            InputLabelProps={{
+              shrink: true,
+            }}
+            value={formData.endDate}
+            onChange={handleInputChange}
+          />
+          <TextField
+            required
+            margin="dense"
+            id="joinFee"
+            name="joinFee"
+            label="Join Fee"
+            type="number"
+            fullWidth
+            variant="standard"
+            value={formData.joinFee}
+            onChange={handleInputChange}
+          />
+          <TextField
+            required
+            margin="dense"
+            id="pricePool"
+            name="pricePool"
+            label="Prize Pool"
+            type="number"
+            fullWidth
+            variant="standard"
+            value={formData.pricePool}
+            onChange={handleInputChange}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleEditClose}>Cancel</Button>
+          <Button type="submit">Edit Tournament</Button>
+        </DialogActions>
+      </Dialog>
+
+      {/* Edit Dialog Box */}
+
+
+      {/* Delete Dialog Box       */}
+      <Dialog
+  open={deleteConfirmationOpen}
+  onClose={handleDeleteConfirmationClose}
+>
+  <DialogTitle>Delete Tournament</DialogTitle>
+  <DialogContent>
+    <DialogContentText>
+      Are you sure you want to delete this tournament?
+    </DialogContentText>
+  </DialogContent>
+  <DialogActions>
+    <Button onClick={handleDeleteConfirmationClose} color="primary">
+      Cancel
+    </Button>
+    <Button onClick={handleDeleteConfirmation} color="primary">
+      Delete
+    </Button>
+  </DialogActions>
+</Dialog>
+
+
+
       <Dialog
         open={open}
         onClose={handleClose}
@@ -222,6 +407,19 @@ const Tournament = (props) => {
             value={formData.joinFee}
             onChange={handleInputChange}
           />
+          <TextField
+            required
+            margin="dense"
+            id="priceMoney"
+            name="priceMoney"
+            label="Price Pool"
+            type="number"
+            fullWidth
+            variant="standard"
+            value={formData.priceMoney}
+            onChange={handleInputChange}
+          />
+
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose}>Cancel</Button>
@@ -240,6 +438,8 @@ const Tournament = (props) => {
                 <TableCell style={{ ...cellStyles, maxWidth: '200px' }}>Start Date</TableCell>
                 <TableCell style={{ ...cellStyles, maxWidth: '200px' }}>End Date</TableCell>
                 <TableCell style={{ ...cellStyles, maxWidth: '200px' }}>Join Fee</TableCell>
+                <TableCell style={{ ...cellStyles, maxWidth: '200px' }}>Price Pool</TableCell>
+                <TableCell style={{ ...cellStyles, maxWidth: '200px' }}>In Tournament</TableCell>
                 <TableCell style={{ ...cellStyles, maxWidth: '200px' }}>Users Joined</TableCell>
                 <TableCell style={cellStyles}>Actions</TableCell> {/* Add Actions column */}
               </TableRow>
@@ -249,13 +449,17 @@ const Tournament = (props) => {
                 <TableRow key={tournament.id}>
                   <TableCell style={cellStyles}>{index + 1}</TableCell>
                   <TableCell style={cellStyles}>{tournament.title}</TableCell>
-                  <TableCell style={cellStyles}>{new Date(tournament.startDate).toLocaleDateString('en-GB')}</TableCell>
+                  <TableCell style={cellStyles}>{new Date(tournament.startDate).toLocaleString('en-GB')}</TableCell>
                   <TableCell style={cellStyles}>{new Date(tournament.endDate).toLocaleString('en-GB')}</TableCell>
                   <TableCell style={{ ...cellStyles, maxWidth: '200px' }}>{tournament.joinFee}</TableCell>
-                  <TableCell style={cellStyles}>N/A</TableCell>
+                  <TableCell style={cellStyles}>{tournament.priceMoney}</TableCell>
+                  <TableCell style={cellStyles}>{tournament.targetAudience}</TableCell>
+                  <TableCell style={cellStyles}>
+                    {tournament.isUserJoined ? "Yes" : "No"}
+                  </TableCell>
                   <TableCell style={cellStyles}> {/* Add Actions buttons */}
-                    <Button onClick={() => handleEdit(tournament)}>Edit</Button>
-                    <Button onClick={() => handleDelete(tournament)}>Delete</Button>
+                    <Button onClick={() => handleEditOpen(tournament)}>Edit</Button>
+                    <Button onClick={() => handleDeleteConfirmation(tournament)}>Delete</Button>
                   </TableCell>
                 </TableRow>
               ))}
